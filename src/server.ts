b0f8379814b8f1a478549c5b2e098c9aed5c8431
RicottaM <jaskulskim.gdynia@gmail.com>
2025-05-18 23:10:22 +0200

    Configured cors options

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,20 @@ import 'module-alias/register';
 import 'dotenv/config';
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import logger from '@/config/logger';
 import pointRouter from './api/routes/point.routes';
 import categoryRouter from '@/api/routes/category.routes';
 import authRouter from '@/api/routes/auth.routes';
 import { errorHandler } from '@/middleware/errorHandler';
+import { corsOptions } from '@/config/corsOptions';
 
 const port = process.env.PORT || 3000;
 const mode = process.env.NODE_ENV || 'development';
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
