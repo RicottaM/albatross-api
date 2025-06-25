562cb3c88f6058b1778f4d1235283d71e0fb696f
RicottaM <jaskulskim.gdynia@gmail.com>
2025-06-25 19:02:08 +0200

    Added update point schema, updated seed

--- src/api/routes/point.routes.ts
+++ src/api/routes/point.routes.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import { PointController } from '@/api/controllers/point.controller';
 import { checkToken } from '@/middleware/checkToken';
 import { validate } from '@/middleware/validate';
-import { pointSchema } from '@/api/schemas/point.schema';
+import { createPointSchema, updatePointSchema } from '@/api/schemas/point.schema';
 
 const pointRouter = Router();
 const controller = container.resolve(PointController);
@@ -11,8 +11,8 @@ const controller = container.resolve(PointController);
 pointRouter.get('/', checkToken, controller.getAll.bind(controller));
 pointRouter.get('/user', checkToken, controller.getByUser.bind(controller));
 pointRouter.get('/:id', checkToken, controller.getById.bind(controller));
-pointRouter.post('/', checkToken, validate(pointSchema), controller.create.bind(controller));
-pointRouter.put('/:id', checkToken, validate(pointSchema), controller.update.bind(controller));
+pointRouter.post('/', checkToken, validate(createPointSchema), controller.create.bind(controller));
+pointRouter.patch('/:id', checkToken, validate(updatePointSchema), controller.update.bind(controller));
 pointRouter.delete('/:id', checkToken, controller.delete.bind(controller));
 
 export default pointRouter;
